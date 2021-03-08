a4110e054e436a3865644f1dfad546062229c88a
Mahmoudibrahim54 <60746387+Mahmoudibrahim54@users.noreply.github.com>
2021-03-08 23:31:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,17 +24,18 @@ app.post("/", function(req, res){
   const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="+ cityName +"&appid=d58bbf3343bf7ea9376f675f3ac00ae2";
 
   https.get(url, function(response) {
-    
+
       response.on("data", function(data) {
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp
             const wDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
-            const image = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            const image = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             res.write("<h1>the temprature in "+ cityName +" is " + temp + " degrees clusius </h1>");
             res.write("<h3>weather description is " + wDescription + "</h3>");
             res.write("<img src=" + image + ">");
             res.send();
+
    });
 
   });
@@ -46,6 +47,6 @@ app.post("/", function(req, res){
 
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("app is active on port 3000");
 });
